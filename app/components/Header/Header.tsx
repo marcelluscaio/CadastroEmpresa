603f247a5748b76e13fde27d78a410e9d61ee4a7
Caio Cabral <ocaiocabral@gmail.com>
2023-10-18 16:02:56 -0400

    feat: header for desktop version

--- app/components/Header/Header.tsx
+++ app/components/Header/Header.tsx
@@ -1,16 +1,29 @@
 import Link from "next/link";
 import Image from "next/image";
+import "./style.scss";
+
 const Header = () => {
 	return (
-		<header>
-			<Link href="/">
-				<Image
-					src="/logo_opea.svg"
-					width={107}
-					height={32}
-					alt="Logo da Opea"
-				/>
-			</Link>
+		<header className="header">
+			<div className="container">
+				<Link href="/">
+					<Image
+						src="/logo_opea.svg"
+						width={107}
+						height={32}
+						alt="Logo da Opea"
+					/>
+				</Link>
+				<figure>
+					<figcaption className="primary--3--bold">Nome do usuário</figcaption>
+					<Image
+						src="/icon-user.svg"
+						width={24}
+						height={24}
+						alt="Foto do usuário"
+					/>
+				</figure>
+			</div>
 		</header>
 	);
 };
