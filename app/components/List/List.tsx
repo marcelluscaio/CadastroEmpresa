f27a71efdc59ca05570de95aaf00a459eddeed8c
Caio Cabral <ocaiocabral@gmail.com>
2023-10-20 00:39:29 -0400

    feat: fetching direclit on list component

--- app/components/List/List.tsx
+++ app/components/List/List.tsx
@@ -1,18 +1,39 @@
 import "./styles.scss";
+import { useState, useEffect } from "react";
+
+type Data = {
+	id: string;
+	name: string;
+	email: string;
+	cnpj: string;
+};
 
 const List = () => {
+	const [data, setData] = useState<Data[]>([]);
+
+	useEffect(() => {
+		fetch("https://outros.opea-uat.solutions/prova/front/api/clients")
+			.then((result) => result.json())
+			.then((result) => setData(result));
+	}, []);
+
 	return (
 		<ul className="company-list">
-			<li className="round-corner">
-				<article>
-					<p className="primary">Nome da Empresa S.A</p>
-					<div className="company-info">
-						<p className="primary--2">CNPJ: 02.564.000/0001-10</p>
-						<span className="primary--1"> - </span>
-						<address className="primary--1">Email: empresa@opea.com</address>
-					</div>
-				</article>
-			</li>
+			{data.map((item) => (
+				<li
+					className="round-corner"
+					key={item.id}
+				>
+					<article>
+						<p className="primary">{item.name}</p>
+						<div className="company-info">
+							<p className="primary--2">CNPJ: {item.cnpj}</p>
+							<span className="primary--1"> - </span>
+							<address className="primary--1">Email: {item.email}</address>
+						</div>
+					</article>
+				</li>
+			))}
 		</ul>
 	);
 };
